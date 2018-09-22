e00acb0f78f6e8a36b79da2ab787f024e5f7eefa
kevin.anderson <nlaoshi830.gmail.com>
2018-09-22 16:12:05 +0900

    Add report create

--- src/controllers/report/ReportsNewServlet.java
+++ src/controllers/report/ReportsNewServlet.java
@@ -9,7 +9,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import models.Report;
-import utils.DBUtil;
 /**
  * Servlet implementation class ReportsNewServlet
  */
